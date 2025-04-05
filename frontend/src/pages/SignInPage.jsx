0dbc5ddaba1b1d22c43f7a1d087788ee086f2091
Michelle Boutilier <cybermomma74@outlook.com>
2025-04-05 09:31:09 -0300

    RegisterPageCSS

--- frontend/src/pages/SignInPage.jsx
+++ frontend/src/pages/SignInPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function SignInPage() {
   return (
     <div>
       <h2>Sign In</h2>
       <p>This is the Sign In Page.</p>
+
+      <Link to="/" style={{ color: '#ec008c', textDecoration: 'underline' }}>
+        ← Back to Home
+      </Link>
     </div>
   );
 }
